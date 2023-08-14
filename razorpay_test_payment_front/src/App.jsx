4cf9e0231c37ba74f28652c0669428ebcb5e5df8
zishan344 <zishanahmed344@gmail.com>
2023-08-14 10:30:00 +0600

    added frontend

---
+++ razorpay_test_payment_front/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2>Welcome to Payment tester project</h2>
+      <button>Pay</button>
+    </div>
+  );
+}
+
+export default App;
